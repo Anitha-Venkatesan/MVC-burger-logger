71f70be123fa6fe57def695ce209b9b096135ce4
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-27 08:53:27 -0500

    replaced ?? with * in select query

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,20 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// Object for all our SQL statement functions.
 var orm = {
-      selectAll: function(whatToSelect, tableInput) {
-        var queryString = "SELECT ?? FROM ??";
-        connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
-          if (err) throw err;
-          console.log(result);
-        });
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
       }
-    }
+      cb(result);
+    });
+  }
+};
+      
+        
+
     
     
 module.exports =orm;
