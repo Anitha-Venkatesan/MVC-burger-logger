54a90f456a7343c0a181df66aa51ae312f56f235
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-28 17:16:49 -0500

    added on click function to devour the burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,8 +3,8 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
     var newBurger = {
-      burger_name: $("#burger").val(),
-      //devoured: $("[name=devoured]:false").val()
+      burger_name: $("#burger").val()
+      //devoured: $("#devourIt").val()
     };
     // Send the POST request.
     $.ajax("/api/burgers", {
@@ -17,5 +17,20 @@ $(function() {
       }
     );
   });
+  //devoured the burger after cicking the button
+  $("button[name='devoured']").on("click", function(event) {
+    var id = $(this).attr("value");
+    var devouredTheBurger = {
+        devoured: true
+    };
+        // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredTheBurger
+    }).then(function() {
+        console.log("Devoured the burger", devouredTheBurger);
+        // Reload the page to get the updated list
+        location.reload();
+    });
+  });
 });
-  
