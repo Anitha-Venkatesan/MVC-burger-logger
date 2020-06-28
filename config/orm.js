7b5dfd6b799b9669a003806cda20b6f4177a21e7
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-28 16:41:26 -0500

    added update orm query

--- config/orm.js
+++ config/orm.js
@@ -40,9 +40,9 @@ function objToSql(ob) {
 
 
 var orm = {
-  selectAll: function(tableInput, cb) {
+  selectAll: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
@@ -50,7 +50,7 @@ var orm = {
     });
   },
   // The variables cols and vals are arrays.
-  insertOne: function(table, cols, vals, cb) {
+  insertOne: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -62,14 +62,28 @@ var orm = {
 
     console.log(queryString);
 
-    connection.query(queryString, vals, function(err, result) {
+    connection.query(queryString, vals, function (err, result) {
       if (err) {
         throw err;
       }
-      console.log(result);    
+      console.log(result);
+      cb(result);
+    });
+  },
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
       cb(result);
     });
   }
 };
-   
-module.exports =orm;
+module.exports = orm;
