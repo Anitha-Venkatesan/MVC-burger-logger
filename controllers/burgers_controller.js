224b3e362c3481cff5aaaf80bada4c471d6e3057
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-29 14:41:08 -0500

    error 404 page, now set to 200

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function (req, res) {
     },
     condition,
     function (result) {
-      if (result.changedRows === 0) {
+      if (result.affectedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
