bacb90f2a4ea153b07e29db21739e2cfb7721b6b
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-29 22:17:59 -0500

    renaming the environment variable

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
 //Setting up mysql connection
 var mysql = require("mysql");
 var connection;
-if (process.env.JawsDB_URL) {
-  connection = mysql.createConnection(process.env.JawsDB_URL);
-  
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.MYSQL_PASSWORD,
-  database: "burgers_db"
-});
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "burgers_db"
+  });
 };
 //Making connection
 connection.connect(function(err) {
